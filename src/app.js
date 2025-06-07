c3ed6fc393d5b994decca8a78836881b491c9c5d
AdrianAguinaga <adrian.aguinaga@uabc.edu.mx>
2025-06-07 11:50:44 -0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+
+
+function init() {
+    console.log("App initialized");
+    //load data
+
+    //hook events
+   
+}
+
+window.onload = init;// this waits for the DOM to be fully loaded before running init 
+//it waits until the html and css are fully loaded before running the init function
