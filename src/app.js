06010e74aa15069b13f957aa353d9109b4d641c5
AdrianAguinaga <adrian.aguinaga@uabc.edu.mx>
2025-06-10 20:09:57 -0700

    session3

--- src/app.js
+++ src/app.js
@@ -1,10 +1,43 @@
+function saveTask() {
+    console.log("Save button clicked");
+    //get the values from the form
+    const title = $("#txtTitle").val();
+    const description = $("#txtDescription").val();
+    const color = $("#selColor").val();
+    const date = $("#selDate").val();
+    const status = $("#selStatus").val();
+    const budget = $("#numBudget").val();
+    console.log(title, description, color, date, status, budget);
+    //build the task object
+    let newTask = new Task(title, description, color, date, status, budget);
+    console.log(newTask);
+    //send the task object to the server
+    
 
+    //display the task in the list section
+    displayTask(newTask);
+
+
+}
+function displayTask(task) 
+{
+    //this is the homework
+    let taskHtml = `<div class="task ">
+        <h3>${task.title}</h3>
+        <p>${task.description}</p>
+        <p>Date: ${task.date}</p>
+        <p>Status: ${task.status}</p>
+        <p>Budget: ${task.budget}</p>`
+    $(".get-list").append(taskHtml);
+    
+}
 
 function init() {
     console.log("App initialized");
     //load data
 
     //hook events
+    $("#btnSave").click(saveTask);
    
 }
 
